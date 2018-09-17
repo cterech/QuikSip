8c4f0adc6af1b1fe24b4e6eb78ac2b4ac29bfb6d
Chris Terech <christerech@0587397806.wireless.umich.net>
2018-09-17 13:14:17 -0400

    Initial Commit

---
+++ QuikSip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuikSip
+//
+//  Created by Chris Terech on 9/17/18.
+//  Copyright © 2018 Chris Terech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
